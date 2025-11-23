440b56500a44fc7057e29b3a6a5c25b94febcdd8
dfn <doof@kali>
2025-11-24 02:23:39 +0700

    fix bugs

--- app/Http/Controllers/ReceiptController.php
+++ app/Http/Controllers/ReceiptController.php
@@ -6,7 +6,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Auth;
 use Barryvdh\DomPDF\Facade\Pdf;
-
 use App\Models\Rekening;
 use App\Models\Kredit;
 use App\Models\Deposito;
