506ea3467d9e4c6832c2fbd4ee373bf0cbd9d73f
Sandriana-Dewi <sandriana.dewi5263@student.unri.ac.id>
2025-11-23 17:38:21 +0700

    Perbarui fitur edit profile: controller, view, dan routes

--- routes/web.php
+++ routes/web.php
@@ -32,44 +32,39 @@ use App\Http\Controllers\NasabahController;
         Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
         Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-        //route untuk nasabah
         Route::get('/nasabah/create', [NasabahController::class, 'create'])->name('nasabah.create');
         Route::post('/nasabah',       [NasabahController::class, 'store'])->name('nasabah.store');
         Route::resource('nasabah', NasabahController::class)->only(['update','show','edit']);
+        Route::get('/nasabah/{nasabah}/preview', [NasabahController::class, 'previewKtp'])->name('nasabah.preview');
+        Route::get('/nasabah/{nasabah}/bukti', [NasabahController::class, 'downloadKtp'])->name('nasabah.bukti');
+        Route::get('/nasabah/{nasabah}/edit', [NasabahController::class, 'edit'])->name('nasabah.edit');
+        Route::put('/nasabah/{nasabah}', [NasabahController::class, 'update'])->name('nasabah.update');
 
         Route::get('/pengajuan/{type}/{id}/detail', [DashboardController::class, 'showDetail'])->name('nasabah.detail');
         Route::delete('/pengajuan/{type}/{id}/cancel', [DashboardController::class, 'cancelPengajuan'])->name('nasabah.cancel');
 
         Route::middleware('can:submit-applications')->group(function () {
-        // Rekening
             Route::get('/rekening/create', [RekeningController::class, 'create'])->name('rekening.create');
             Route::post('/rekening',       [RekeningController::class, 'store'])->name('rekening.store');
             Route::get('/rekening/{rekening}/edit', [RekeningController::class, 'edit'])->name('rekening.edit');
             Route::patch('/rekening/{rekening}', [RekeningController::class, 'update'])->name('rekening.update');
 
-            // Kredit
             Route::get('/kredit/create', [KreditController::class, 'create'])->name('kredit.create');
             Route::post('/kredit', [KreditController::class, 'store'])->name('kredit.store');
             Route::get('/kredit/{kredit}/edit', [KreditController::class, 'edit'])->name('kredit.edit');
             Route::patch('/kredit/{kredit}', [KreditController::class, 'update'])->name('kredit.update');
 
-            // Deposito
             Route::get('/deposito/create', [DepositoController::class, 'create'])->name('deposito.create');
             Route::post('/deposito',       [DepositoController::class, 'store'])->name('deposito.store');
             Route::get('/deposito/{deposito}/edit', [DepositoController::class, 'edit'])->name('deposito.edit');
             Route::patch('/deposito/{deposito}', [DepositoController::class, 'update'])->name('deposito.update');
         });
-        
-        // // Receipt
-        // Route::get('/pengajuan/cetak/{tipe}/{id}', [ReceiptController::class, 'cetak'])->name('pengajuan.cetak');
-    
-        // Kredit
+            
         Route::get('/kredit/{kredit}/bukti', [KreditController::class, 'downloadBukti'])
             ->name('kredit.bukti');
         Route::get('/kredit/{kredit}/preview', [KreditController::class, 'previewBukti'])
             ->name('kredit.preview');
 
-        // Deposito
         Route::get('/deposito/{deposito}/bukti', [DepositoController::class, 'downloadBukti'])
             ->name('deposito.bukti');
         Route::get('/deposito/{deposito}/preview', [DepositoController::class, 'previewBukti'])
@@ -92,12 +87,10 @@ use App\Http\Controllers\NasabahController;
         Route::get('/pengajuan/{type}/{id}/download', [PengajuanController::class,'download'])->name('pengajuan.download');
     });
 
-    // Guest auth (custom tambahan di luar auth.php)
     Route::middleware('guest')->group(function () {
         Route::get('register', [RegisteredUserController::class, 'create'])->name('register');
         Route::post('register', [RegisteredUserController::class, 'store']);
 
-        // OTP (kalau memang kamu tambahkan di controller itu)
         Route::get('verify-otp',  [RegisteredUserController::class, 'showVerifyForm'])->name('verify.otp.form');
         Route::post('verify-otp', [RegisteredUserController::class, 'verifyOTP'])->name('verify.otp');
         Route::post('resend-otp', [RegisteredUserController::class, 'resendOTP'])->name('resend.otp');
